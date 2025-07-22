6ab8db2d2472abd545cb4873ae21ed11adc8d817
sowgro <tpoke.ferrari@gmail.com>
2025-07-22 02:22:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# nautilus-newfile
+Adds a "New File..." option to the context menu of nautilus
+
+### Installation
+Copy both `nautilus-newfile.py` and `new-file-dialog.ui` to `~/.local/share/nautilus-python/extensions`
+
+### Limitations
+- No filename validation is run (needs implementation)
+- Modal is not attached to the nautilus window (limitation of the nautilus extensions api)
+- The newly created file is not automatically selected (limitation of the nautilus extensions api)
---
+++ nautilus-newfile.py
@@ -0,0 +1,37 @@
+import os, gi
+gi.require_version('Adw', '1')
+gi.require_version("Gtk", "4.0")
+from gi.repository import Nautilus, GObject, Adw, Gtk, Gio
+from typing import List
+
+@Gtk.Template(filename=os.path.join(__file__, "../new-file-dialog.ui"))
+class NewFileDialog(Adw.Dialog):
+    fileinfo:Gio.FileInfo = None
+    __gtype_name__ = "NewFileDialog"
+    name_entry: Adw.EntryRow = Gtk.Template.Child()
+
+    def __init__(self, fileinfo):
+        super().__init__()
+        self.fileinfo = fileinfo
+
+    @Gtk.Template.Callback()
+    def on_create_clicked(self, _):
+        f: Gio.File = (self.fileinfo.get_location())
+        f.get_child(self.name_entry.get_text()).create(Gio.FileCreateFlags(0), None)
+        self.close()
+
+class NautilusNewfile(GObject.GObject, Nautilus.MenuProvider):
+    def show_dialog(self, _, fileinfo):
+
+        dialog = NewFileDialog(fileinfo)
+        dialog.present()
+
+    def get_background_items(self, fileinfo: Gio.FileInfo) -> List[Nautilus.MenuItem]:
+
+        menuitem = Nautilus.MenuItem(
+            name="ExampleMenuProvider::Foo2",
+            label="New File...",
+        )
+        menuitem.connect('activate', self.show_dialog, fileinfo)
+
+        return [menuitem,]
